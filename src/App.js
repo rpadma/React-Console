5972c5264230b60f9334229bdcda4ab8b90f5955
Rohit Padma <rohitp32@gmail.com>
2018-01-08 13:35:50 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -14,9 +14,12 @@ class App extends Component {
     this.clearHistory=this.clearHistory.bind(this);
     this.addHistory=this.addHistory.bind(this);
     this.ViewLink=this.ViewLink.bind(this);
-    this.registerCommands=this.registerCommands.bind(this);
+    this.showCommands=this.showCommands.bind(this);
     this.showHelp=this.showHelp.bind(this);
     this.showIntroMsg=this.showIntroMsg.bind(this);
+    this.showProject=this.showProject.bind(this);
+    this.sendEmail=this.sendEmail.bind(this);
+    this.downloadResume=this.downloadResume.bind(this);
     
 
 this.state={ commands:{},
@@ -50,7 +53,7 @@ this.state={ commands:{},
 componentDidMount() {
 
  const term= ReactDOM.findDOMNode(this)
- this.registerCommands();
+ this.showCommands();
  this.showIntroMsg();
  this.term.focus();
 
@@ -88,14 +91,20 @@ clearHistory() {
   
 }
 
-registerCommands() {
+
+
+showCommands() {
   this.setState({
     commands: {
       'clear' : this.clearHistory,
       'help'  : this.showHelp,
       'github': this.ViewLink('https://github.com/rpadma'),
-      'portfolio': this.ViewLink('http://rohitpadma.me'
-    )
+      'portfolio': this.ViewLink('http://rohitpadma.me'),
+      'aboutme' : this.showIntroMsg,
+      'resume': this.downloadResume,
+      'email' : this.sendEmail,
+      'projects' :this.showProject
+    
     }
   });
 }
@@ -118,10 +127,30 @@ registerCommands() {
 }
 
 showHelp() {
-  this.addHistory("help - this  is help text");
+  this.addHistory("help - this is help text to know all available options");
+  this.addHistory("aboutme - to know me")
   this.addHistory("github - view github profile");
   this.addHistory("portfolio - view my portfolio");
+  this.addHistory("resume - To download my resume");
+  this.addHistory("email - leave a message ");
+  this.addHistory("projects - Projects list");
   this.addHistory("clear - clear screen");
+}
+
+showProject(){
+
+  this.addHistory(" Still working on this");
+}
+
+sendEmail(){
+
+  this.addHistory("Still working on this");
+
+}
+
+downloadResume(){
+
+  this.addHistory("Still working on this");
 }
 
 
@@ -129,8 +158,8 @@ showHelp() {
 
   render(){
 console.log(this.state.history);
- let output = this.state.history.map((op,index) => {
-     return <p key={index}>{op}</p>
+ let output = this.state.history.map((op) => {
+     return <p>{op}</p>
   });
 
     return(
